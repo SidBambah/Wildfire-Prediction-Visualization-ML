460e393618267954973d06838b1d64ebd3a49601
Sidharth Bambah <sids53@gmail.com>
2019-11-21 22:40:00 -0500

    Added live API endpoint

--- frontend/src/components/APIConnection.js
+++ frontend/src/components/APIConnection.js
@@ -2,6 +2,9 @@
 // Connection to REST API
 
 const APIConnection = {
-    "endpoint": "http://127.0.0.1:5000/api"
+    //Local
+    //"endpoint": "http://127.0.0.1:5000/api"
+    //Live
+    "endpoint": "http://big-data-env2.us-east-1.elasticbeanstalk.com/api"
 }
 export default APIConnection
