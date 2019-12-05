a0b664dcf834663576dadddb4ef7bd763177962b
Sidharth Bambah <sids53@gmail.com>
2019-12-05 16:36:18 -0500

    Added month and day of week suggestions to form

--- frontend/src/components/PredictionForm.js
+++ frontend/src/components/PredictionForm.js
@@ -34,11 +34,34 @@ class PredictionForm extends React.Component {
             <div className="col">
               <input type="text" className="form-control" placeholder= "Location" name="location" id="autocomplete" onChange = {this.changeHandler} />
             </div>
+            <datalist id="months">
+              <option value="1">January</option>
+              <option value="2">February</option>
+              <option value="3">March</option>
+              <option value="4">April</option>
+              <option value="5">May</option>
+              <option value="6">June</option>
+              <option value="7">July</option>
+              <option value="8">August</option>
+              <option value="9">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </datalist>
             <div className="col">
-              <input type="text" className="form-control" placeholder= "Month" name="month" onChange = {this.changeHandler} />
+              <input list="months" type="text" className="form-control" placeholder= "Month" name="month" onChange = {this.changeHandler} />
             </div>
+            <datalist id="dayofweek">
+              <option value="1">Monday</option>
+              <option value="2">Tuesday</option>
+              <option value="3">Wednesday</option>
+              <option value="4">Thursday</option>
+              <option value="5">Friday</option>
+              <option value="6">Saturday</option>
+              <option value="7">Sunday</option>
+            </datalist>
             <div className="col">
-              <input type="text" className="form-control" placeholder= "Day of Week" name="dayofweek" onChange = {this.changeHandler} />
+              <input list="dayofweek" type="text" className="form-control" placeholder= "Day of Week" name="dayofweek" onChange = {this.changeHandler} />
             </div>
             <div className="col">
               <input type="button" className="btn btn-primary" value="Make Prediction" onClick={this.handleSubmit}/>
